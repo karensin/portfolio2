22a81aedd17c2524fb8c8fddcd5e881b5da9e53e
Jason Jiang <jjiang1991@gmail.com>
2019-12-19 13:31:39 -0800

    Functional bubbls
    bobbles around and grows
    
    pretty bubbles

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Dimensions from 'react-dimensions';
 
-import * as firebase from 'firebase';
 import './Projects.css';
 
 class Square extends Component {
-    constructor(props){
-      super(props);
-    }
     render (){
-       
         return (
-
             <div className= "wrapper"> 
                 <a href = {this.props.linkSrc} > 
                     <div className={this.props.className}
