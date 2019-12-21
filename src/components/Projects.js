124926175eae7d947e839973616ea798b2ba52f7
Jason Jiang <jjiang1991@gmail.com>
2019-12-21 11:44:22 -0800

    made squares not stupid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,34 +6,27 @@ import Square from './Square.js';
 
 
 class Projects extends Component {
-  constructor (props){
-    super(props);
-    this.state = {};
-  }
+    constructor (props){
+        super(props);
+        this.state = {};
+    }
   
-  render() {
-    return (
-      <Container className=".mt-4">
-        <Row className = 'projectsBox'>
-          <Col>
-            <Square className="projectBox" imgSrc = {process.env.PUBLIC_URL + "/images/MeeseeksBox.jpg" } linkSrc= "https://www.facebook.com/meeseeksBox2019">
-              <div className="projectName">Meeseeks Bot</div>
-            </Square>
-          </Col>
-          <Col>
-            <Square className="projectBox">
-              <div className="projectName"> asdf </div>
-            </Square>
-          </Col>
-          <Col>
-            <Square className="projectBox">
-              <div className="projectName"> asdf </div>
-            </Square>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+    render() {
+        return <Container className=".mt-4">
+            <Row className='projectsBox'>
+                <Col>
+                    <Square className="projectBox" imgSrc={process.env.PUBLIC_URL + "/images/MeeseeksBox.jpg"}
+                            linkSrc="https://www.facebook.com/meeseeksBox2019" projName="Meeseeks Bot"/>
+                </Col>
+                <Col>
+                    <Square className="projectBox"/>
+                </Col>
+                <Col>
+                    <Square className="projectBox"/>
+                </Col>
+            </Row>
+        </Container>
+    }
 }
 
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -5,15 +5,18 @@ import './Projects.css';
 
 class Square extends Component {
   render (){
+    // const squareSize = this.props.containerWidth * 0.8;
+    const squareSize = '300px';
+    // const left = this.props.containerWidth - 150;
     return (
-
         <a href = {this.props.linkSrc} className={this.props.className}
             style={{
-              width: this.props.containerWidth * 0.8, 
-              height: this.props.containerWidth * 0.8,
-              left: this.props.containerWidth * 0.1,
+              width: squareSize,
+              height: squareSize,
+              // left: this.props.containerWidth * 0.1,
               backgroundImage:"url(" + this.props.imgSrc + ")"
-            }}> 
+            }}>
+            <div className="projectName">{this.props.projName}</div>
             {this.props.children}
         </a>
 
